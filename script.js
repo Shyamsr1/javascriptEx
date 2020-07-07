74fac47472431fe942609773cb017501c89906c0
Shyamsr1 <63706177+Shyamsr1@users.noreply.github.com>
2020-07-07 08:40:13 +0530

    Modified index.html content as well.

--- script.js
+++ script.js
@@ -371,3 +371,8 @@ function openCity(evt, cityName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
